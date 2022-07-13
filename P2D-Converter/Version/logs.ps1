5cffe24430c839260ece1354508b7c9bc1607d8e
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-07-12 21:29:51 -0500

    Delete P2D-Converter directory

--- P2D-Converter/Version/logs.ps1
+++
@@ -1,6 +0,0 @@
-$logs = @(
-"V1.0       Initial Release       7-July-2022",
-"V1.1       Optimized Update functionality and added logging       9-July-2022",
-"V1.2       Filtered upload setting to only allow ps1 files       11-July-2022",
-"V1.3       Made the Version a global variable       12-July-2022"
-)
