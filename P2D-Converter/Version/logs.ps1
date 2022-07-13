a9c19b9ca267ee5586f03aa9c5c626b50a617a92
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-07-12 20:56:59 -0500

    Update logs.ps1

--- P2D-Converter/Version/logs.ps1
+++ P2D-Converter/Version/logs.ps1
@@ -1,6 +1,6 @@
 $logs = @(
 "V 1.0      Initial Release          7 July 2022",
-"V 1.1      Optimized Update functionality and added logging         9 July 2022",
-"V 1.2      Filtered upload setting to only allow ps1 files          11 July 2022",
-"V 1.25     Made the version a global variable                       12 July 2022"
+"V 1.1      Optimized Update functionality and added logging 9 July 2022",
+"V 1.2      Filtered upload setting to only allow ps1 files 11 July 2022",
+"V 1.25     Made the version a global variable 12 July 2022"
 )
